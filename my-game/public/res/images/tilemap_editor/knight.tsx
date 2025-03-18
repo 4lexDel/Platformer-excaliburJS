0ef9fbb9291921db21e546c946c7ce730feb98b5
DELORME Alexandre <alexandre.delorme@rte-france.com>
2025-03-18 21:00:43 +0100

    last updates

--- my-game/public/res/images/tilemap_editor/knight.tsx
+++ my-game/public/res/images/tilemap_editor/knight.tsx
@@ -1,5 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="2025.03.14" name="knight" tilewidth="32" tileheight="32" tilecount="64" columns="8" tilerendersize="grid" fillmode="preserve-aspect-fit">
+<tileset version="1.10" tiledversion="2025.03.14" name="knight" tilewidth="32" tileheight="32" tilecount="64" columns="8" fillmode="preserve-aspect-fit">
+ <tileoffset x="0" y="4"/>
+ <grid orientation="orthogonal" width="64" height="64"/>
  <image source="../#brackeys_platformer_assets/sprites/knight.png" width="256" height="256"/>
  <tile id="4">
   <animation>
