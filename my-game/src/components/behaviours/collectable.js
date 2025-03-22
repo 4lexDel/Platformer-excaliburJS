862952a49784ac6c910df698097356a47c5684c6
DELORME Alexandre <alexandre.delorme@rte-france.com>
2025-03-22 19:49:35 +0100

    add build content

--- my-game/src/components/behaviours/collectable.js
+++
@@ -1,9 +0,0 @@
-import { Component } from "excalibur"
-
-/**
- * Represents an entity that can be picked up by the player
- */
-export class CollectableComponent extends Component {
-  isCollected = false
-  isCollectable = true
-}
